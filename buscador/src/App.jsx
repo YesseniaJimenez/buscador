c67d69281b7a7c5464289f37628cac619f75dcbe
yessenia <yejiga2006@gmail.com>
2023-04-19 20:22:42 -0500

    inicio de proyecto

---
+++ buscador/src/App.jsx
@@ -0,0 +1,20 @@
+const people = [
+  { id: "people-01", title: "juan Perez" },
+  { id: "people-02", title: "Danny Ocean" },
+  { id: "people-03", title: "Benito Martinez" },
+  { id: "people-04", title: "Austin Santos" },
+  { id: "people-05", title: "Fercho" },
+];
+const calendar = [
+  { id: "calendar-01", title: "sesión de seguiomiento" },
+  { id: "calendar-01", title: "sesión de seguiomiento" },
+  { id: "calendar-01", title: "sesión de seguiomiento" },
+  { id: "calendar-01", title: "sesión de seguiomiento" },
+  { id: "calendar-01", title: "sesión de seguiomiento" },
+];
+
+function App() {
+  return <div>hola mundo</div>;
+}
+
+export default App;
